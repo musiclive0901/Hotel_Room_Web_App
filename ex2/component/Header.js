0dc8e40ad67d47da96583a1658ed7ec885634a54
Nam Ho <namhonhat96@gmail.com>
2020-07-11 12:48:22 -0500

    Remove unecessary exercises
    Remove unecessary exercises

--- ex2/component/Header.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Header() {
-  return <head className="header_border">This is header</head>;
-}
-
-export default Header;
--- ex5/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-import MyInfo from "./component/MyInfo.js";
-
-ReactDOM.render(<MyInfo />, document.getElementById("root"));
